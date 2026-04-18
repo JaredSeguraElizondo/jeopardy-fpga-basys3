45f4c7d381664fa15c457ec913d36ce2f47ee112
Jared Segura Elizondo <jaredsegura2505@gmail.com>
2026-04-18 23:45:28 +0000

    Arreglando comentarios

--- SV_code/Modulos/pwm_buzzer.sv
+++ SV_code/Modulos/pwm_buzzer.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/24/2026 06:54:06 PM
-// Design Name: 
-// Module Name: pwm_buzzer
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-`timescale 1ns / 1ps
 
 module pwm_buzzer (
     input  logic        clk_i,
