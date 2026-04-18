acccada8cce1dcca06354e0c590b4d09eeb94363
Jared Segura Elizondo <jaredsegura2505@gmail.com>
2026-04-18 23:46:46 +0000

    Arreglando comentarios

--- SV_code/Modulos/sistema_de_sonido_FSM.sv
+++ SV_code/Modulos/sistema_de_sonido_FSM.sv
@@ -1,26 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/06/2026 05:48:22 PM
-// Design Name: 
-// Module Name: sistema de sonido FSM
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-`timescale 1ns / 1ps
 
 module sound_manager (
     input  logic clk_i,
@@ -30,9 +8,7 @@ module sound_manager (
     output logic speaker_pwm_o  // Va directo al pin de la placa (J1)
 );
 
-    // =========================================================
-    // BANCO DE FRECUENCIAS (A 16 MHz)
-    // =========================================================
+    
     localparam [15:0] TONE_CORRECT = 16'd8000;  // Sonido Agudo (~1000 Hz)
     localparam [15:0] TONE_WRONG   = 16'd53000; // Sonido Grave (~150 Hz)
 
@@ -44,9 +20,6 @@ module sound_manager (
         else           current_period = TONE_WRONG;
     end
 
-    // =========================================================
-    // INSTANCIA DEL GENERADOR PWM (Tu código original)
-    // =========================================================
     pwm_buzzer buzzer_inst (
         .clk_i    (clk_i),
         .rst_i    (rst_i),
