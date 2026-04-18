015b6c39ee648b6bd47b9e058b11101a3b35d708
Jared Segura Elizondo <jaredsegura2505@gmail.com>
2026-04-18 23:41:50 +0000

    Arreglando comentarios

--- SV_code/Modulos/LFSR.sv
+++ SV_code/Modulos/LFSR.sv
@@ -1,26 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/06/2026 04:53:43 PM
-// Design Name: 
-// Module Name: LFSR
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-`timescale 1ns / 1ps
 
 module lfsr (
     input  logic       clk_i,
